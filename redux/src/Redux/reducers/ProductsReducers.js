4c756f2af7d679490533ff92cb058416326a3bb9
Samanvith <Samanvith2005@gmail.com>
2023-10-13 10:39:32 +0530

    connected Redux with react

--- redux/src/Redux/reducers/ProductsReducers.js
+++ redux/src/Redux/reducers/ProductsReducers.js
@@ -1,17 +1,20 @@
-import {Actiontypes} from "../Constants/action-types"
-const initialstate ={
-     products: [
-        { 
-            name:"Samanvith",
-            id:1,
-            category:"student"
-    } ]
-}
-export const productreducer =(state=initialstate,{type,payload})=>{
- switch(type){
+import { Actiontypes } from "../Constants/action-types";
+
+const initialState = {
+  products: [
+    {
+      name: "Samanvith",
+      id: 1,
+      category: "student",
+    },
+  ],
+};
+
+export const productReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
     case Actiontypes.SET_PRODUCTS:
-        return {...state,products:payload}
-        default:
-        return state;
- };
-}
+      return { ...state, products: payload };
+    default:
+      return state;
+  }
+};
